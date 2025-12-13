3bb6ee28d485d2cbfd104d184e3a7c68620f17a8
xHlpNw <aleksashin990@gmail.com>
2025-12-13 05:11:45 +0400

    database connection

---
+++ src/main/java/ru/teamscore/ConsoleRunner.java
@@ -0,0 +1,13 @@
+package ru.teamscore;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsoleRunner implements CommandLineRunner {
+    @Override
+    public void run(String[] args) {
+
+    }
+}
