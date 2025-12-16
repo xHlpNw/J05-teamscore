3aac7a63119512829984fefba0a2ffa2de5d2b97
xHlpNw <aleksashin990@gmail.com>
2025-12-16 15:37:16 +0400

    console interface for task2

--- src/main/java/ru/teamscore/Main.java
+++ src/main/java/ru/teamscore/Main.java
@@ -1,6 +1,7 @@
 package ru.teamscore;
 
 import ru.teamscore.Task1.FlightDAO;
+import ru.teamscore.Task2.BookingDAO;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
@@ -12,6 +13,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         FlightDAO dao = new FlightDAO();
 
+        System.out.println("Task1");
         System.out.print("Введите трёхбуквенный код аэропорта: ");
         String airportCode = scan.nextLine().trim();
 
@@ -33,5 +35,31 @@ public class Main {
                 System.out.println(flight);
             }
         }
+
+        System.out.println("\nTask2");
+        System.out.print("Введите номер рейса: ");
+        int flightNo = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("Введите класс обслуживаня (Economy/Comfort/Business): ");
+        String fareConditions = scan.nextLine();
+
+        LocalDate bookDate = null;
+        while (bookDate == null) {
+            System.out.print("Введите дату бронирования (YYYY-MM-DD): ");
+            String dateInput = scan.nextLine().trim();
+            try {
+                bookDate = LocalDate.parse(dateInput);
+            } catch (DateTimeParseException e) {
+                System.out.println("Некорректный формат даты.");
+            }
+        }
+
+        System.out.print("Введите новую цену: ");
+        double price = scan.nextDouble();
+
+        BookingDAO bookingDao = new BookingDAO();
+        bookingDao.updatePrice(flightNo, fareConditions, bookDate, price);
+        System.out.println("Данные обновлены");
     }
 }
